80a910e7fe237d11e485d62cc56e716970a55a4d
Spirrwell <Spirrwell@gmail.com>
2022-12-20 22:36:02 -0500

    Bit more robust with --overwrite support

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,72 @@
 #include <filesystem>
 #include <string_view>
 #include <iostream>
+#include <vector>
+#include <unordered_set>
 
 #define REPO "https://github.com/Spirrwell/Template_CMake.git"
+#define VERSION "0.0.1"
 
 int main( int argc, char* argv[] )
 {
+	std::unordered_set<std::string_view> arguments;
+
 	if ( argc > 1 )
 	{
 		for ( int i = 1; i < argc; ++i )
-		{
-			if ( std::string_view( argv[i] ) == "--version" )
-			{
-				std::cout << "0.0.1\n";
-				return 0;
-			}
-		}
+			arguments.emplace( argv[i] );
+	}
+
+	if ( arguments.contains( "--version" ) )
+	{
+		std::cout << VERSION << '\n';
+		return 0;
 	}
 
 	const int result = system( "git clone --depth=1 " REPO " template-cmake" );
 
-	if ( result != 0 ) {
+	if ( result != 0 )
+	{
 		std::cout << "Failed to clone repo\n";
 		return result;
 	}
 
+	const bool should_overwrite = arguments.contains( "--overwrite" );
+
 	std::filesystem::remove_all( "template-cmake/.git" );
-	const std::filesystem::directory_iterator it( "./template-cmake" );
 
-	for ( const std::filesystem::path& p : it )
+	std::vector<std::filesystem::path> files;
+
+	const std::filesystem::recursive_directory_iterator it( "./template-cmake" );
+	for ( const std::filesystem::path& path : it )
 	{
-		if ( p.filename() == ".gitignore" )
-			continue;
+		if ( std::filesystem::is_directory( path ) )
+		{
+			const std::filesystem::path dest = path.lexically_relative( "./template-cmake/" );
+			std::filesystem::create_directories( dest );
+		}
+		else if ( path.filename() != ".gitignore" )
+			files.emplace_back( path );
+	}
 
-		const std::filesystem::path dest = "." / p.filename();
-		std::filesystem::rename( p, dest );
+	for ( const auto& path: files )
+	{
+		const std::filesystem::path dest = path.lexically_relative( "./template-cmake/" );
+		bool should_move = true;
+
+		if ( std::filesystem::exists( dest ) )
+		{
+			if ( !should_overwrite )
+			{
+				std::cout << "Skipping " << path.generic_string() << '\n';
+				should_move = false;
+			}
+			else
+				std::cout << "Overwriting " << path.generic_string() << '\n';
+		}
+
+		if ( should_move )
+			std::filesystem::rename( path, dest );
 	}
 
 	std::filesystem::remove_all( "template-cmake/" );
